0824d8fb370306fc5cb5646a999c188f4bc875f9
Harsh Amol Deshpande <39777192+harshdesh7@users.noreply.github.com>
2018-12-10 13:33:36 -0700

    This Python script tests whether API calls return expected results

--- milestone_5_tests.py
+++ milestone_5_tests.py
@@ -1,20 +1,21 @@
 import requests
 import unittest
 
+#These Test API calls, see if they return correct values
 class TestAPICalls(unittest.TestCase):
-
+	#first call tests a recipe get call, checks if recipe with id 410 call gives back expected result
 	def test_recipe(self):
 		resp = requests.get('https://djibouti-recipe.herokuapp.com/api/recipes/410')
 		for res in resp.json():
 			print(res['name'])
 			self.assertEqual(res['name'], "F in the Chat");
-
+	#second tests checks one of the ingredients (id = 20)
 	def test_ingredient(self):
 		resp = requests.get('https://djibouti-recipe.herokuapp.com/api/ingredients/20')
 		for res in resp.json():
 			print(res['name'])
 			self.assertEqual(res['name'], "waffles");
-
+	#third test checks on of the users (id = 17)
 	def test_user(self):
 		resp = requests.get('https://djibouti-recipe.herokuapp.com/api/users/17')
 		for res in resp.json():
